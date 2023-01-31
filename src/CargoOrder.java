b633b128f1f4ad226a9eb08b18dbc24f51c4d288
Nicolas Jager <nicowebsitemail@gmail.com>
2023-01-30 23:15:30 -0500

    Added getCargoOrder method to generate a random CargoOrder and return it.  Added toString() method override for CargoOrder.  Moved test code to a seperate function runTests.

--- src/CargoOrder.java
+++ src/CargoOrder.java
@@ -1,4 +1,8 @@
 package traingame;
 
 public record CargoOrder(Product product, City destination, int payment) {
+	@Override
+	public String toString() {
+		return String.format("%-15s %-20s %-5s%n", product.name().toString(), destination.name().toString(), payment);
+	}
 }
