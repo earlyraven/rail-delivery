a475e5ca6886ca5d4d41d32fad4b4d6a420b040a
Nicolas Jager <nicowebsitemail@gmail.com>
2023-02-01 01:38:58 -0500

    Simplified CargoOrder creation by using Product.getRandom() and inlining the return statement. Improved readability by replacing spaces with tabs for better consistency within the file and moving a comment.

--- src/CargoOrder.java
+++ src/CargoOrder.java
@@ -2,19 +2,18 @@ package traingame;
 
 import java.util.Random;
 
+//NOTE: For now, keeping it simple.  Just random.  Might want to re-balance payout later.
+//For example, to care about the distance between pickup and delivery cities.
 public record CargoOrder(Product product, City destination, int payment) {
-    //NOTE: For now, keeping it simple.  Just random.  Might want to re-balance payout later.
-    //For example, to care about the distance between pickup and delivery cities.
 	public static CargoOrder getRandom(City[] cities) {
-        //Chose a random product, city and payout.
-        Random random = new Random();
-        Product chosenProduct = Product.valueOf(Product.getRandom().name());
-        City chosenCity = cities[random.nextInt(cities.length)];
-        int chosenPayout = random.nextInt(20,50);
+	//Chose a random product, city and payout.
+	Random random = new Random();
+	Product chosenProduct = Product.getRandom();
+	City chosenCity = cities[random.nextInt(cities.length)];
+	int chosenPayout = random.nextInt(20,50);
 
-        //Create and return the CargoOrder.
-        CargoOrder cargoOrder = new CargoOrder(chosenProduct, chosenCity, chosenPayout);
-        return cargoOrder;
+	//Create and return the CargoOrder.
+	return new CargoOrder(chosenProduct, chosenCity, chosenPayout);
 	}
 
 	@Override
